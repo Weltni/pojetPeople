fc6bea19321d31c4b8efa81582886f4ae4b92bf2
Weltni <vandemaele.alexis@gmail.com>
2023-04-27 11:03:08 +0200

    ajout du back

---
+++ projetpeople/src/main/java/com/projetpeople/projetpeople/ProjetpeopleApplication.java
@@ -0,0 +1,13 @@
+package com.projetpeople.projetpeople;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetpeopleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetpeopleApplication.class, args);
+	}
+
+}
---
+++ projetpeople/src/test/java/com/projetpeople/projetpeople/ProjetpeopleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetpeople.projetpeople;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetpeopleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
