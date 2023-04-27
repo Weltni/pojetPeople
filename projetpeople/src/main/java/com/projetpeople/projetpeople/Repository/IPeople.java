d910ecf3016d8e1b7afa9bde81b2952286607d41
Weltni <vandemaele.alexis@gmail.com>
2023-04-27 15:24:01 +0200

    Rajout de quelque function

--- projetpeople/src/main/java/com/projetpeople/projetpeople/Repository/IPeople.java
+++ projetpeople/src/main/java/com/projetpeople/projetpeople/Repository/IPeople.java
@@ -2,6 +2,15 @@ package com.projetpeople.projetpeople.Repository;
 
 import com.projetpeople.projetpeople.Models.People;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface IPeople  extends JpaRepository<People, Integer> {
+
+    List<People> findBylastnameStartingWith(String lastName);
+
+
+
 }
--- projetpeople/src/main/java/com/projetpeople/projetpeople/controller/people.java
+++ projetpeople/src/main/java/com/projetpeople/projetpeople/controller/people.java
@@ -6,16 +6,15 @@ import com.projetpeople.projetpeople.Repository.IPeople;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @Component
 @RequestMapping("/people")
+@SpringBootApplication
 // @CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 public class people {
 
@@ -23,10 +22,63 @@ public class people {
       private IPeople peopleRepo;
 
 
-      @GetMapping("/test")
-      public List<People> getAllPeople() {
-     return peopleRepo.findAll();
+
+
+    @GetMapping("/test")
+    public List<People> getAllPeople() {
+        return peopleRepo.findAll();
     }
 
+
+    @DeleteMapping("{peopleid}")
+    public void deletePeople(@PathVariable("peopleid") Integer id){
+
+          peopleRepo.deleteById(id);
+    }
+
+
+   record  NewPeople(
+           String firstname,
+           String lastname,
+           String email,
+           String phone,
+           String country
+
+   ){}
+
+    @PostMapping
+    public void addPeople(@RequestBody NewPeople request){
+        People people = new People();
+        people.setFirstname(request.firstname());
+        people.setLastname(request.lastname());
+        people.setEmail(request.email());
+        people.setPhone(request.phone());
+        people.setCountry(request.country());
+       peopleRepo.save(people);
+    }
+
+    @GetMapping("/find/{id}")
+    public Optional<People> findPeople(@PathVariable Integer id){
+        return peopleRepo.findById(id);
+    }
+
+    @GetMapping("/test/{lastname}")
+    public List<People> getPeopleByName(@PathVariable String lastname) {
+
+        return peopleRepo.findBylastnameStartingWith(lastname);
+    }
+    @PutMapping("/{peopleid}")
+    public void updatePeople(@PathVariable("peopleid") Integer id, @RequestBody People peopleDetail) {
+
+        People updatePeople = peopleRepo.findById(id).orElseThrow();
+
+        updatePeople.setFirstname(peopleDetail.getFirstname());
+        updatePeople.setLastname(peopleDetail.getLastname());
+        updatePeople.setEmail(peopleDetail.getEmail());
+        updatePeople.setPhone(peopleDetail.getPhone());
+        updatePeople.setCountry(peopleDetail.getCountry());
+
+        peopleRepo.save(updatePeople);
+    }
 
 }
